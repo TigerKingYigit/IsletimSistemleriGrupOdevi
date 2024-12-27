81f5e2b836188949c33fa26ea4d88f2856bf097d
Yavuz Selim Say <119061563+YvzSlmSy@users.noreply.github.com>
2024-12-27 21:13:06 +0300

    Yavuz Selim Say
    PipeFonkOrder fonksiyonu eklendi

--- main.c
+++ main.c
@@ -95,3 +95,174 @@ int fileOutput(char* command[], char* outputFile) {
         }
     }
 }
+
+
+
+int pipeFonkOrder(char* command[]){
+
+    int i = 0;
+    int numberOfCommands = 0;
+    while(command[i] != NULL){
+        if(command[i] == "+")
+            numberOfCommands++;
+
+        i++;
+    }
+    pid_t pid1 = fork();
+
+    if(pid1 == 0){
+
+        char* command1[5];
+        int r = 0;
+        i = 0;
+        int isSpecial1 = 0;
+        int isSpecial2 = 0;
+        int isSpecial3 = 0;
+        while(command[i] != "+"){
+        
+            if(strcmp(command[i], ">") == 0){
+                isSpecial1 = 1;
+                break;
+            }
+            if(strcmp(command[i], "<") == 0){
+                isSpecial2 = 1;
+                break;
+            }
+            if(strcmp(command[r], "|") == 0){
+                isSpecial3 = 1;
+                break;
+            }
+            command1[r] = command[i];
+
+            r++;
+            i++;
+        }
+
+        command1[r] = NULL;
+
+        if(isSpecial1 == 1){
+            int err = fileOutput(command1, command[i+1]);
+        }
+        else if(isSpecial2 == 1){
+            int err = fileInput(command1, command[i+1]);
+        }
+        else{
+            int err = execvp(command1[0], command1);
+        }
+
+        exit(0);
+    }
+    else if (pid1 > 0) {
+        // Ebeveyn süreç
+        wait(NULL); // İlk çocuğun bitmesini bekleyin
+
+        pid_t pid2 = fork();
+        if (pid2 == 0) {
+            
+            char* command2[5];
+            int r = 0;
+            i = 0;
+            int isSpecial1 = 0;
+            int isSpecial2 = 0;
+            int isSpecial3 = 0;
+
+            while(command[i] != "+"){
+                i++;
+            }
+            i++;
+            
+            while(command[i] != "+"){
+                if(strcmp(command[i], ">") == 0){
+                    isSpecial1 = 1;
+                    break;
+                }
+                if(strcmp(command[i], "<") == 0){
+                    isSpecial2 = 1;
+                    break;
+                }
+                if(strcmp(command[r], "|") == 0){
+                    isSpecial3 = 1;
+                    break;
+                }
+                
+                command2[r] = command[i];
+
+                r++;
+                command2[r] = NULL;
+                i++;
+            }
+
+            if(isSpecial1 == 1){
+                fileOutput(command2, command[i+1]);
+            }
+            else if(isSpecial2 == 1){
+                fileInput(command2, command[i+1]);
+            }
+            else{
+                int err = execvp(command2[0], command2);
+            }
+
+            exit(0);
+        } else if (pid2 > 0) {
+            wait(NULL); // İkinci çocuğun bitmesini bekleyin
+
+            if(numberOfCommands == 3){
+                char* command3[5];
+                int r = 0;
+                i = 0;
+                int isSpecial1 = 0;
+                int isSpecial2 = 0;
+                int isSpecial3 = 0;
+
+                while(command[i] != "+"){
+                    i++;
+                }
+                i++;
+                while(command[i] != "+"){
+                    i++;
+                }
+                i++;
+
+                while(command[i] != "+"){
+
+                    if(strcmp(command[i], ">") == 0){
+                        isSpecial1 = 1;
+                        break;
+                    }
+                    if(strcmp(command[i], "<") == 0){
+                        isSpecial2 = 1;
+                        break;
+                    }
+                    if(strcmp(command[r], "|") == 0){
+                        isSpecial3 = 1;
+                        break;
+                    }
+                    
+                    command3[r] = command[i];
+
+                    r++;
+                    command3[r] = NULL;
+                    i++;
+                }
+
+                if(isSpecial1 == 1){
+                    fileOutput(command3, command[i+1]);
+                }
+                else if(isSpecial2 == 1){
+                    fileInput(command3, command[i+1]);
+                }
+                else{
+                    int err = execvp(command3[0], command3);
+                }
+                
+            }
+
+            exit(0);
+        } else {
+            perror("Second fork failed");
+        }
+    } else {
+        perror("First fork failed");
+    }
+    return 0;
+}
